d87c1609c6a0afbeb3aa67caf0d5f0159a2ae2fc
Markus Loeffler <m_loeffler@gmx.de>
2023-03-11 16:43:53 +0000

    cleanup

--- src/main/java/com/maloef/cdnd/capstone/config/CorsConfig.java
+++ src/main/java/com/maloef/cdnd/capstone/config/CorsConfig.java
@@ -1,18 +1,12 @@
 package com.maloef.cdnd.capstone.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CapstoneConfig {
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
