36582f4d090c05c3643b8794b157c99f029bc171
Markus Loeffler <m_loeffler@gmx.de>
2023-03-15 12:01:05 +0100

    try to fix github build

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // export const apiEndpoint = `http://a2a7529a340d94d96b47de57c88c9666-189089577.eu-central-1.elb.amazonaws.com:8080`
-export const apiEndpoint = `http://localhost:8080`
+export const apiEndpoint = `http://localhost:8080` 
 
 export const authConfig = {
   domain: 'dev-b5l8pxg7z3snujdy.us.auth0.com',            // Auth0 domain
