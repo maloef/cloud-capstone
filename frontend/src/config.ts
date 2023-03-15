294590592abff7ad8a0ffecc96852d60591426c5
Markus Loeffler <m_loeffler@gmx.de>
2023-03-15 16:51:33 +0100

    update url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -4,6 +4,6 @@ export const apiEndpoint = `http://a2cbb9cea6b0541e1b2d9f59e5ab7e36-1453076124.e
 export const authConfig = {
   domain: 'dev-b5l8pxg7z3snujdy.us.auth0.com',            // Auth0 domain
   clientId: 'K0KPyXXrmwDLRNiIrBulZYq93NROLedq',          // Auth0 client id
-  callbackUrl: 'http://a932eac94ad504c3fac624609a6db908-318301360.eu-central-1.elb.amazonaws.com:' // frontend url
+  callbackUrl: 'http://a932eac94ad504c3fac624609a6db908-318301360.eu-central-1.elb.amazonaws.com/callback' // frontend url
   // callbackUrl: 'http://localhost:3000/callback'
 }
