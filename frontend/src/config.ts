850bdcaba19cf499589bac53670d0ff30cc91056
Markus Loeffler <m_loeffler@gmx.de>
2023-03-15 11:55:52 +0100

    dummy commit

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,8 +2,6 @@
 export const apiEndpoint = `http://localhost:8080`
 
 export const authConfig = {
-  // DONE: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-b5l8pxg7z3snujdy.us.auth0.com',            // Auth0 domain
   clientId: 'K0KPyXXrmwDLRNiIrBulZYq93NROLedq',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
