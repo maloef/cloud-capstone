b4df30de2311a18c4fae1800e3184eae014c16dc
Markus Loeffler <m_loeffler@gmx.de>
2023-03-15 12:44:11 +0100

    add ymls, update backend endpoint

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-// export const apiEndpoint = `http://a2a7529a340d94d96b47de57c88c9666-189089577.eu-central-1.elb.amazonaws.com:8080`
-export const apiEndpoint = `http://localhost:8080`    
+export const apiEndpoint = `http://a2cbb9cea6b0541e1b2d9f59e5ab7e36-1048984640.eu-central-1.elb.amazonaws.com:8080`
+// export const apiEndpoint = `http://localhost:8080`
 
 export const authConfig = {
   domain: 'dev-b5l8pxg7z3snujdy.us.auth0.com',            // Auth0 domain
